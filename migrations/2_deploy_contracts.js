244fe449ff038ee488501315dc61ed92dbf36db9
Andrew Chan <46029314+AndrewChan168@users.noreply.github.com>
2019-10-12 16:14:36 +0800

    remove unnecessary and add new contract BuyerData
    - remove contract timestamp setting
    - move some functionalities to new contract BuyerData

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,6 +3,7 @@
 //const FlightData = artifacts.require("FlightData");
 //const InsuranceData = artifacts.require("InsuranceData");
 const FlightSuretyData = artifacts.require("FlightSuretyData");
+const BuyerData = artifacts.require("BuyerData");
 //const OracleManagement = artifacts.require("OracleManagement");
 const FlightSuretyApp = artifacts.require("FlightSuretyApp");
 //const Authorizable = artifacts.require("Authorizable");
@@ -16,6 +17,9 @@ module.exports = function(deployer) {
   //deployer.deploy(OracleManagement);
   //deployer.deploy(FlightSuretyApp);
   deployer.deploy(FlightSuretyData).then(()=>{
-    return deployer.deploy(FlightSuretyApp, FlightSuretyData.address)
+    //return deployer.deploy(FlightSuretyApp, FlightSuretyData.address)
+    return deployer.deploy(BuyerData)
+  }).then(()=>{
+    return deployer.deploy(FlightSuretyApp, FlightSuretyData.address, BuyerData.address);
   })
 };
