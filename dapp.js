842d0c69bdaeb88cb4f35a49d55b0bde9ab0c820
Andrew Chan <46029314+AndrewChan168@users.noreply.github.com>
2019-10-15 18:43:57 +0800

    Add better comments

--- dapp.js
+++ dapp.js
@@ -1,3 +1,9 @@
+/**
+ * Server that connecting UI to Dapp. It involves two parts: 1) web-socket & web-server.
+ * The web-socket part use socket-io to push smart contract events to UI as messages.
+ * The web server part supports GET & POST function from UI and interact with Dapp
+ */
+
 const path = require('path');
 
 const Web3 = require("web3");
@@ -12,18 +18,11 @@ const app = express();
 const server = http.createServer(app);
 var socketIO = require('socket.io')(server);
 
+// define PORT and gas-price as constant
 const PORT = 8000;
 const gasPrice = web3.utils.toWei('0.00001', 'ether');
 
-/** setup websocket server */
-
-socketIO.on('connection', socket=>{
-    console.log(`A user connected`);
-    socket.on('disconnect', ()=>{
-        console.log('user disconnected');
-    })
-})
-
+// set dapp instances for web-socket and web-server
 const FlightSuretyApp = require(path.join(__dirname, "build/contracts/FlightSuretyApp.json"));
 const FlightSuretyData = require(path.join(__dirname, "build/contracts/FlightSuretyData.json"));
 const BuyerData = require(path.join(__dirname, "build/contracts/BuyerData.json"));
@@ -31,6 +30,18 @@ let flightSuretyData = new web3.eth.Contract(FlightSuretyData.abi, Config.dataAd
 let buyerData = new web3.eth.Contract(BuyerData.abi, Config.buyerDataAddress);
 let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, Config.appAddress);
 
+/**
+ * Web-Socket part. Use socket of web3 instances to listen on events. 
+ * Once events arrival, use socket-io emit message to UI
+ */
+// listening to socket connection from UI
+socketIO.on('connection', socket=>{
+    console.log(`A user connected`);
+    socket.on('disconnect', ()=>{
+        console.log('user disconnected');
+    })
+})
+// listening to events from Dapp
 flightSuretyApp.events.allEvents()
     .on('data', (event)=>{
         console.log(event);
@@ -45,12 +56,24 @@ flightSuretyData.events.allEvents()
     })
     .on('error', (error)=>rej(error));
 
+buyerData.events.allEvents()
+.on('data', (event)=>{
+    console.log(event);
+    socketIO.emit('event emit', {event:event.event, content:event.returnValues});
+})
+.on('error', (error)=>rej(error));
 
-
+/**
+ * Web-server that support UI functionalities. There are 3 pages in UI and nav-bar on UI.
+ * 1) Flights UI: Showing all available flights and passenager's flights. It also provide flight status fetching function
+ * 2) Insurance UI: Showing all insurances of the passenager and provide button for withdrawing credits
+ * 3) Airline UI: Showing all registered airlines status and their fund
+ */
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// API supporting all passenagers available for selection on nav-bar
 app.get("/passenagers", async(req, res)=>{
     let accounts = await web3.eth.getAccounts()
     let passenagers = accounts.slice(31,40);
@@ -58,8 +81,13 @@ app.get("/passenagers", async(req, res)=>{
     res.status(200).send(passenagers);
 });
 
+/**
+ * API functions supporting Flights UI
+ * 
+ */
+
+// API support all-flight tables
 app.get("/flights/all/:sender", async(req, res)=>{
-    //console.log(`Get all flights`)
     var allFlights = [];
     try{
         const sender = req.params.sender;
@@ -85,6 +113,7 @@ app.get("/flights/all/:sender", async(req, res)=>{
     }
 });
 
+// API support fetch flight status button
 app.post("/flights/fetch", async(req, res)=>{
     const cost = await flightSuretyApp.methods
                         .fetchFlightStatus(
@@ -104,6 +133,7 @@ app.post("/flights/fetch", async(req, res)=>{
             .send({from:req.body.sender,gas:cost+5000, gasPrice:gasPrice});
 });
 
+// API support flights table of the selected passenager
 app.get("/flights/passenager/:sender", async(req, res)=>{
     var allFlights = [];
     try{
@@ -130,6 +160,7 @@ app.get("/flights/passenager/:sender", async(req, res)=>{
     }
 })
 
+// API support button buying insurance for flight
 app.post("/insurances/buy", async (req, res)=>{
     console.log("buy insurance for flight");
     try{
@@ -147,6 +178,12 @@ app.post("/insurances/buy", async (req, res)=>{
     }
 });
 
+/**
+ * API functions supporting Insurances UI
+ * 
+ */
+
+// API support passenager's insurances table 
 app.get("/insurance/passenager/:sender", async(req, res)=>{
     var allInsurances = [];
     try{
@@ -174,6 +211,7 @@ app.get("/insurance/passenager/:sender", async(req, res)=>{
     }
 });
 
+// API support card showing passenager's current credits
 app.get("/insurance/credits/fetch/:sender", async(req, res)=>{
     try{
         const credits = await buyerData.methods
@@ -186,6 +224,7 @@ app.get("/insurance/credits/fetch/:sender", async(req, res)=>{
     }
 })
 
+// API support withdraw button for withdrawing passenager's credits
 app.post("/insurance/credits/withdraw", async(req, res)=>{
     try{
         const cost = await flightSuretyApp.methods
@@ -200,6 +239,7 @@ app.post("/insurance/credits/withdraw", async(req, res)=>{
     }
 });
 
+// API support routing the main HTML to UI
 app.use("/", async(req, res) => {
     console.log(`Root`)
     const appValid = await flightSuretyApp.methods.isOperational().call({from:Config.contractOwner});
